474a60d4cbc884422fb9a89d46464bb7965788a9
James Hughes <james@pyrosoftsolutions.co.uk>
2023-03-13 02:48:38 +0000

    Added DI Extension tests and modified CD to allow for package splitting

--- src/CelesTrakSdk.Microsoft.Extensions.DependencyInjection/DependencyInjection/DependencyInjectionExtensions.cs
+++ src/CelesTrakSdk.Microsoft.Extensions.DependencyInjection/DependencyInjection/DependencyInjectionExtensions.cs
@@ -1,4 +1,5 @@
 using CelesTrakSdk.Internal.Clients;
+using CelesTrakSdk.Public.Clients;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CelesTrakSdk.Microsoft.Extensions.DependencyInjection.DependencyInjection;
@@ -7,7 +8,7 @@ public static class DependencyInjectionExtensions
 {
 	public static IServiceCollection AddCelesTrakServices(this IServiceCollection services)
 	{
-		services.AddHttpClient<CelesTrakClient>(h => h.BaseAddress = new("https://celestrak.org/satcat/records.php"));
+		services.AddHttpClient<ICelesTrakClient, CelesTrakClient>(h => h.BaseAddress = new("https://celestrak.org/satcat/records.php"));
 
 		return services;
 	}
